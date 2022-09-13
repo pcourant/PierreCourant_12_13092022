2e021c4c8e674eda192f246b8a1c2fe0ed5a9d40
Pierre COURANT <pcourant.dev@gmail.com>
2022-09-13 21:40:01 +0200

    Add styled-component CSS-in-JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 10em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const App = () => {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <Title>Hello world!</Title>
     </div>
   );
 };
