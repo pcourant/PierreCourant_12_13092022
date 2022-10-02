c0a3bd57514f1490f9001ada520391030146a676
Pierre COURANT <pcourant.dev@gmail.com>
2022-10-02 12:25:50 +0200

    Add(hooks & chart): JSDoc

--- src/utils/charts/index.js
+++ src/utils/charts/index.js
@@ -1,26 +1,51 @@
 import { select } from 'd3';
 
+// Constants from figma mockups
 export const RECT_DIMENSION_RATIO = 0.383;
 export const SQUARE_DIMENSION_RATIO = 1.0194;
 export const KEYINFO_DIMENSION_RATIO = 0.4806;
 export const SIDEBAR_DIMENSION_RATIO = 0.4806;
 
-// Constants from figma mockups
+/**
+ * Scale the mockup value to the current chart width
+ * @param {Number} mockupValue
+ * @param {Number} currentChartWidth
+ * @param {Number} mockupChartWidth
+ * @returns {Number} the scaled value
+ */
 export function prorataScale(mockupValue, currentChartWidth, mockupChartWidth) {
   return (currentChartWidth * mockupValue) / mockupChartWidth;
 }
 
 const WINDOW_ORIGINAL_WIDTH = 1440;
-export function prorataWindowScale(mockupValue, currentChartWidth) {
-  return prorataScale(mockupValue, currentChartWidth, WINDOW_ORIGINAL_WIDTH);
+/**
+ * Scale the mockup value to the window width
+ * @param {Number} mockupValue
+ * @param {Number} windowCurrentWidth
+ * @returns {Number} the scaled value
+ */
+export function prorataWindowScale(mockupValue, windowCurrentWidth) {
+  return prorataScale(mockupValue, windowCurrentWidth, WINDOW_ORIGINAL_WIDTH);
 }
 
 const RECTCHART_ORIGINAL_WIDTH = 835;
+/**
+ * Scale the mockup value to the rectangle chart width
+ * @param {Number} mockupValue
+ * @param {Number} currentChartWidth - the rectangle chart width
+ * @returns {Number} the scaled value
+ */
 export function scaleRectChart(mockupValue, currentChartWidth) {
   return prorataScale(mockupValue, currentChartWidth, RECTCHART_ORIGINAL_WIDTH);
 }
 
 const SPAN1_CHART_ORIGINAL_WIDTH = 258;
+/**
+ * Scale the mockup value to the chart width
+ * @param {Number} mockupValue
+ * @param {Number} currentChartWidth - the chart width
+ * @returns {Number} the scaled value
+ */
 export function scale1spanChart(mockupValue, currentChartWidth) {
   return prorataScale(
     mockupValue,
@@ -29,8 +54,13 @@ export function scale1spanChart(mockupValue, currentChartWidth) {
   );
 }
 
-export const wrap = function (text, lineHeight) {
-  text.each(function () {
+/**
+ * Wrap a SVG <text> element in multiple lines if necessary
+ * @param {SVG <text> element} textSVG - an SVG <text> element to be wrapped
+ * @param {Number} lineHeight - the height of the text lines
+ */
+export const wrap = function (textSVG, lineHeight) {
+  textSVG.each(function () {
     const text = select(this);
     const words = text.text().split(/\s+/).reverse();
     let word;
@@ -66,6 +96,13 @@ export const wrap = function (text, lineHeight) {
   });
 };
 
+/**
+ * Get the coordinates in the hexagon of the value
+ * @param {Number} value - the value to get the coordinates from
+ * @param {Number} radius - the radius of the hexagon
+ * @param {Number<0-6>} index - a corner of the hexagon
+ * @returns {Object.<x: Number, y: Number>} - coordinates of the value
+ */
 export function getCoordinates(value, radius, index) {
   const angle = Math.PI / 2 + (2 * Math.PI * -index) / HEXAGONAL_POINTS;
   const x = Math.cos(angle) * value;
@@ -75,6 +112,12 @@ export function getCoordinates(value, radius, index) {
 
 const HEXAGONAL_POINTS = 6;
 
+/**
+ * Get the coordinates of the 6 points of an hexagon of a specified radius
+ * @param {Number} hexagonRadius - the radius of the current hexagon
+ * @param {Number} containerRadius - the radius of the biggest hexagon
+ * @returns {Array.<Number[]>}
+ */
 export function getHexagonPoints(hexagonRadius, containerRadius) {
   const hexagonPoints = [];
   for (let i = 0; i < HEXAGONAL_POINTS; i++) {
@@ -85,10 +128,23 @@ export function getHexagonPoints(hexagonRadius, containerRadius) {
   return hexagonPoints;
 }
 
+/**
+ * Transform points coordinates to a String for an SVG <polygon> 'points' attribute
+ * @param {Array.<Number[]>} points - coordinates of all points
+ * @returns {String}
+ */
 export function pointsToPath(points) {
   return points?.map((p) => p.join(',')).join(' ');
 }
 
+/**
+ * Transform the polar coordinates to a cartesian coordinates
+ * @param {Number} centerX
+ * @param {Number} centerY
+ * @param {Number} radius
+ * @param {Number} angleInDegrees
+ * @returns {Object.<x: Number, y: Number>}
+ */
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
 
@@ -98,6 +154,15 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   };
 }
 
+/**
+ * Construct the 'd' attribute of a SVG path element to draw an arc
+ * @param {Number} cx - x coordinate of the center of the circle
+ * @param {Number} cy - x coordinate of the center of the circle
+ * @param {Number} radius - the radius of the circle
+ * @param {Number} startAngle - angle to start the arc
+ * @param {Number} endAngle - angle to end the arc
+ * @returns {String} - the 'd' attribute to draw the arc in a SVG path element
+ */
 export function describeArc(cx, cy, radius, startAngle, endAngle) {
   const start = polarToCartesian(cx, cy, radius, endAngle);
   const end = polarToCartesian(cx, cy, radius, startAngle);
@@ -121,6 +186,12 @@ export function describeArc(cx, cy, radius, startAngle, endAngle) {
   return d;
 }
 
+/**
+ * Format the user performance data
+ * @param {Array.<{value: Number, kind: Integer}>} data
+ * @param {Object.<1: String, 2: String, 3: String, 4: String, 5: String, 6: String>} kind
+ * @returns {Number[]}
+ */
 export function formatPerformanceData(data, kind) {
   if (data && kind) {
     const dataSorted = [
@@ -145,6 +216,11 @@ export function formatPerformanceData(data, kind) {
   }
 }
 
+/**
+ * Format the user activity data
+ * @param {Array.<{day: String, kilogram: Number, calories: Number}>} data
+ * @returns {Array.<{x: Number, y1: Number, y2: Number}>}
+ */
 export function formatActivityData(data) {
   return data?.map((activity) => ({
     x: new Date(activity?.day).getDate(),
@@ -153,6 +229,11 @@ export function formatActivityData(data) {
   }));
 }
 
+/**
+ * Format the user average sessions data
+ * @param {Array.<{day: Integer, sessionLength: Number}>} sessions
+ * @returns {Array.<xTick: String, value: Number>}
+ */
 export function formatAverageSessionsData(sessions) {
   const dayOfWeek = ['D', 'L', 'M', 'M', 'J', 'V', 'S'];
 
--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect, useLayoutEffect } from 'react';
 import axios from 'axios';
 
+/**
+ * Pause the calling function during the specified number of milliseconds
+ * @param {Integer} ms - The number of milliseconds to block the calling function
+ * @returns {Promise} - A promise which will be resolved after the specified number of milliseconds
+ */
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+/**
+ * Custom Hooks that get the width of a DOM element after each window resizing
+ * @param {A useRef() Object} chartContainerRef - a mutable ref of a DOM element
+ * @returns {Number} - The current width of chartContainerRef after the last window resizing
+ */
 export function useUpdateWidth(chartContainerRef) {
   const [width, setWidth] = useState(0);
 
@@ -18,6 +28,10 @@ export function useUpdateWidth(chartContainerRef) {
   return width;
 }
 
+/**
+ * Custom Hooks that get the size of th window after each window resizing
+ * @returns {Number[]} - The current size [width, height] of ths window after the last window resizing
+ */
 export function useWindowSize() {
   const [size, setSize] = useState([0, 0]);
   useLayoutEffect(() => {
@@ -31,6 +45,11 @@ export function useWindowSize() {
   return size;
 }
 
+/**
+ * Custom Hooks that wrap a Axios GET request from a specified URL
+ * @param {String} url - the URL to fetch data from
+ * @returns {Object.<isLoading: Boolean, error: Object, data: Object>}
+ */
 export function useAxiosGet(url) {
   const [data, setData] = useState({});
   const [isLoading, setLoading] = useState(true);
@@ -60,5 +79,5 @@ export function useAxiosGet(url) {
     };
     axiosGet();
   }, [url]);
-  return { isLoading, data, error };
+  return { isLoading, error, data };
 }
